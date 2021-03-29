3ed4e949a2d8356677f70b68186da9f0602ca8a2
JaggedNel <33070237+JaggedNel@users.noreply.github.com>
2021-03-29 07:19:19 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,6 @@
+--Вывести общую сумму продаж с разбивкой по месяцам, за все время работы компании
+
 SELECT DATEADD(MONTH, DATEDIFF(MONTH, 0, s.OrderDate), 0) AS Month, SUM(s.SubTotal) AS Total
 FROM Sales.SalesOrderHeader AS s
 GROUP BY DATEADD(MONTH, DATEDIFF(MONTH, 0, s.OrderDate), 0)
 ORDER BY Month;
-
